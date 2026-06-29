0f8e95d7d9382a601ddd1e960f914ad926f8f9bc
Subhajit-bag <subhajitbag984@gmail.com>
2026-06-30 00:14:24 +0530

    Added new string examples

---
+++ 4_Conditional/If_Else.py
@@ -0,0 +1,24 @@
+# If Else Statements 
+
+# a = int(input("enter your age:"))
+# print("your age is:", a)
+
+# if(a<18):
+#     print("you can't drive")
+# else:
+#     print("you can drive")
+# -----------------------------------------------------------------
+
+# Elif  Statements 
+score = 40
+
+if score > 90:
+  print("Grade: A")
+elif score > 80:
+  print("Grade: B")
+elif score > 70:
+  print("Grade: C")
+elif score > 60:
+  print("Grade: D")
+else:
+  print("Beter Luck Next Time")
