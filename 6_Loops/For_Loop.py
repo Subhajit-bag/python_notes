b7da0b96a309ecc14c8d999b089c9cb583525cba
Subhajit-bag <subhajitbag984@gmail.com>
2026-07-01 22:24:29 +0530

    Updated Python notes in loops

---
+++ 6_Loops/For_Loop.py
@@ -0,0 +1,39 @@
+# fruits = 'apple'
+# for fruit in fruits:
+#     print(fruit) # this data show in 
+
+# --------------------------------------------------------------
+
+# fruits = 'apple'
+# for fruit in fruits:
+#     print(fruit)
+#     if (fruit == "p"):
+#         print("this is a good: ")
+# --------------------------------------------------------------
+
+# x =["red", "blue", "black", "green"]
+# for i in x:
+#     print(i)
+#     for a in i:
+#         print(a)
+# --------------------------------------------------------------
+
+# range() Function
+# for x in range(6):
+#   print(x)
+# --------------------------------------------------------------
+
+
+# for a in range(1, 10):
+#   print(a)
+# --------------------------------------------------------------
+
+# for x in range(2, 30, 3):
+#   print(x)
+# --------------------------------------------------------------
+
+
+a = "i love python"
+for x in range(1, 6):
+  # print(a) # this is a good example of for loop printing the same string multiple times using a for loop
+  print(f"{x}. {a}") # this is a good example of for loop printing the same string multiple times using a for loop with index number
