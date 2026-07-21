d78e67047fc77106f153e8ae6026b56c4028df7e
Subhajit-bag <subhajitbag984@gmail.com>
2026-07-21 12:36:38 +0530

    Updated Python notes in try and except

---
+++ 12_for_Loop_with_else/1.py
@@ -0,0 +1,35 @@
+# for i in range(20):
+#     if i == 17:
+#         print("Found", i)
+#         break
+# else:
+#     print("Not Found")
+
+
+# **************************************
+
+# for i in range(10):
+#     if i == 13:
+#         print("Found", i)
+#         break
+# else:
+#     print("Not Found")
+
+# **************************************
+
+
+# for i in range(1, 21):
+#     if i % 2 == 0 and i % 7 == 0:
+#         print("Found", i)
+#         break
+# else:
+#     print("Not Found")
+
+# **********************************************************
+
+for i in range(1, 51):
+    if i % 5 == 0 and i % 7 == 0:
+        print("Found", i)
+        break
+else:
+    print("Not Found")
